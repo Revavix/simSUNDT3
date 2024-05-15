be7f7091db5b5f340370d0dd2837562b72af6119
Markus Larsson <markus.larsson@revavix.com>
2024-05-15 14:16:35 +0200

    Changes for S3-118 + improve cache list
    S3-118 and S3-90
    
    Also some minor improvements:
    
    Cache list now has an indicator of how many results are in a project
    Dates are fixed, no more unformatted dates in cache list.
    Actual version is now displayed to the right of "SimSUNDT" in the top left corner

--- src/components/Alert.svelte
+++ src/components/Alert.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     export let text: string
     export let color: string
     export let icon: string
+    export let duration: number | null = null
+    let timeLeft: number | null = duration
+
+    onMount(() => {
+        if (duration) {
+            const interval = setInterval(() => {
+                if (timeLeft === null) {
+                    clearInterval(interval)
+                    return
+                }
+
+                timeLeft = timeLeft - 0.1
+                if (timeLeft <= 0) {
+                    clearInterval(interval)
+                }
+            }, 100)
+        }
+    })
 </script>
 
 <div class="flex flex-col">
@@ -12,6 +32,16 @@
         <div class="flex flex-col px-2 my-2">
             <p>{text}</p>
         </div>
+        {#if duration}
+        <!-- Countdown circle -->
+        <div class="flex flex-col ml-auto mr-2">
+            <div class="flex flex-col rounded-full bg-stone-400 w-6 h-6 items-center justify-center">
+                <p class="text-xs">
+                    {timeLeft?.toFixed(0)}
+                </p>
+            </div>
+        </div>
+        {/if}
     </div>
 </div>
 
